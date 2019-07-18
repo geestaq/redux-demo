ecba057872c9d8893f7a57c9e19c25b63c726b41
Rafał Nowakowski <gee@vp.pl>
2019-07-18 15:26:38 +0200

    Add comments removal

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) =>
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) =>
     <li>
-        {text}
-        <span>votes: {votes} </span>
+        {text} <span>votes: {votes} </span>
         <button onClick={() => thumbUpComment(id)}>Thumb up</button>
         <button onClick={() => thumbDownComment(id)}>Thumb down</button>
+        <button onClick={() => removeComment(id)}>Delete</button>
     </li>;
 
 export default Comment;
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,10 +1,11 @@
 import {connect} from 'react-redux';
 import Comment from './Comment';
-import {thumbUpComment, thumbDownComment} from './actions';
+import {thumbUpComment, thumbDownComment, removeComment} from './actions';
 
 const mapDispatchToProps = dispatch => ({
     thumbUpComment: (id) => dispatch(thumbUpComment(id)),
-    thumbDownComment: (id) => dispatch(thumbDownComment(id))
+    thumbDownComment: (id) => dispatch(thumbDownComment(id)),
+    removeComment: (id) => dispatch(removeComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
