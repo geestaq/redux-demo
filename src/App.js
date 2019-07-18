fb8ddab67202aaa1c0dc20ae43125a74e2033a7d
Rafał Nowakowski <gee@vp.pl>
2019-07-18 17:07:33 +0200

    Add redux-devtools

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import CommentListContainer from './CommentListContainer';
 import CommentFormContainer from './CommentFormContainer';
+import DevTools from './DevTools';
 
 const App = () => {
     return (
         <div className="App">
             <CommentListContainer/>
             <CommentFormContainer/>
+            <DevTools/>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+import { createStore } from 'redux';
 import reducer from './reducer';
+import DevTools from './DevTools';
 import { addComment } from  './actions'
 
-const logger = createLogger();
 const store = createStore(
     reducer,
-    applyMiddleware(logger)
+    DevTools.instrument()
 );
 
 ReactDOM.render(
